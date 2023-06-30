bc48b593b5154b1b7a5ae2676d98d21957795ab3
Huy Nguyễn <huyhol287@gmail.com>
2023-06-30 14:56:06 +0700

    add function findByName in ProductDAO

--- src/java/dao/ProductDAO.java
+++ src/java/dao/ProductDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
@@ -38,9 +39,43 @@ public class ProductDAO extends jdbc.DBConnect {
         return products;
     }
     
+    public Vector<Product> getByName(String name) {
+        Vector<Product> products = new Vector<>();
+        String sql = "SELECT [id]\n"
+                + "      ,[categoryId]\n"
+                + "      ,[suplierId]\n"
+                + "      ,[name]\n"
+                + "      ,[description]\n"
+                + "      ,[price]\n"
+                + "      ,[discount]\n"
+                + "      ,[quantity]\n"
+                + "  FROM [dbo].[Product]"
+                + " where name like ?";
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, "%" + name + "%");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                int categoryId = rs.getInt(2);
+                int supplierId = rs.getInt(3);
+                String productName = rs.getString(4);
+                String description = rs.getString(5);
+                double price = rs.getDouble(6);
+                double discount = rs.getDouble(7);
+                int quantity = rs.getInt(8);
+                products.add(new Product(id, categoryId, supplierId, productName, description, price, discount, quantity));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return products;
+    }
+    
     public static void main(String[] args) {
         ProductDAO dao = new ProductDAO();
-        Vector<Product> products = dao.getAll();
+        
+        Vector<Product> products = dao.getByName("White");
         for (Product product : products) {
             System.out.println(product);
         }
