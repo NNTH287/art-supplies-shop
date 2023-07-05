59f19832adcdb4df89371a96d6ac87145cdaf4ca
Huy Nguyễn <huyhol287@gmail.com>
2023-07-05 23:40:23 +0700

    style: remove editor-fold in controllers

--- src/java/controller/HomeController.java
+++ src/java/controller/HomeController.java
@@ -39,7 +39,6 @@ public class HomeController extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
